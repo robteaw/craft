6ec8feb772d62194fc423689de939dd06b905496
Robert Tea <robteaw@gmail.com>
2023-10-21 23:53:16 -0500

    Navbar added

---
+++ src/app/components/Navbar/Navbar.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import styles from "./Navbar.module.scss";
+
+export default function Navbar() {
+  return (
+    <div className={styles.container}>
+      <ul className={styles.links}>
+        <li>
+          <Link href="/login">Login</Link>
+        </li>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/sew">Sew</Link>
+        </li>
+        <li>
+          <Link href="/crotchet">Crotchet</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
---
+++ src/app/crotchet/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../components/Navbar/Navbar";
+
+export default function Crotchet() {
+  return (
+    <div>
+      <Navbar />
+      Crotchet
+    </div>
+  );
+}
+
+// * Next.js + Typescript
+// * classnames library
+// * react hook form + yup
+// * Sass
