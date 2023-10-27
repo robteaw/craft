5cc1851b8e46f7cca5ac2cf7478d8cf8e45794a4
Robert Tea <robteaw@gmail.com>
2023-10-26 23:54:35 -0500

    Loader component added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import Navbar from "./components/Navbar/index";
+
 export default function Home() {
   return (
     <main>
+      <Navbar />
       <div>Home</div>
     </main>
   );
--- src/app/sewing/page.tsx
+++ src/app/sewing/page.tsx
@@ -1,5 +1,14 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
 export default function Sewing() {
-  return <div>Sewing</div>;
+  const [count, setCount] = useState<number>(1);
+
+  return (
+    <div>
+      <h1>Sewing is {count}</h1>
+      <button onClick={() => setCount((prev) => prev + 1)}>+</button>
+      <button onClick={() => setCount((prev) => prev - 1)}>-</button>
+    </div>
+  );
 }
