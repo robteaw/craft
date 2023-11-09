5df628972093a72276566cbf7410481c1e582300
Robert Tea <robteaw@gmail.com>
2023-11-08 23:52:53 -0600

    Images added

--- src/app/crotchet/page.tsx
+++ src/app/crotchet/page.tsx
@@ -1,5 +1,26 @@
-import React from "react";
+"use client";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-export default function Crotchet() {
-  return <div>Crotchet</div>;
+type Props = {
+  children: ReactNode;
+};
+
+export default function Crotchet({ children }: Props) {
+  const [data, setData] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios
+      .get("https://life-leap.onrender.com/api/users/me")
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        navigate("/signup");
+        console.log(error);
+      });
+  }, []);
+  return <>{children}</>;
 }
--- src/app/sewing/page.tsx
+++ src/app/sewing/page.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import Loader from "../components/Loader/index";
 import Navbar from "../components/Navbar";
 import styles from "./Sewing.module.scss";
+// images
+import Image from "next/image";
+import andy from "@/assets/images/andy.png";
+import harry from "@/assets/images/andy.png";
 
 export default function Sewing() {
   return (
@@ -10,7 +14,9 @@ export default function Sewing() {
       <Navbar />
       <h1>Sewing</h1>
       <h3>Andy Barclay</h3>
+      <Image src="/andy.png" width={500} height={500} alt="" />
       <h3>Harry Potter</h3>
+      <Image src="/harry.png" width={500} height={500} alt="" />
     </div>
   );
 }
